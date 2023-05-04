700b8ec7850e468822cc44260c8dee6018c30130
Ирина <papahinairina@gmail.com>
2023-05-04 21:48:42 +0300

    created checked input and toggle switch

--- my-app/src/components/Salary/Salary.js
+++ my-app/src/components/Salary/Salary.js
@@ -1,35 +1,102 @@
-import './style.css';
-
+import React, { useEffect, useState }from "react";
+import './style.css'
 
 
 export default function Salary () {
+  const [monthlySalary, setmonthlySalary] = useState(40000);
+  const [checked, setChecked] = useState('monthlySalary');
+  const [toggle, setToggle] = useState(true);
+  const [salary, setSalary] = useState(monthlySalary);
 
-return (
-  <div className='wrapper'>
-    <div className='container'>
-      <h2> Salary</h2>
-      <div>
-        1
+  const tax = Math.round(monthlySalary * 0.13);
+  const salaryWithoutTax =  Math.ceil(monthlySalary - tax);
+  const mrot =  24801; 
+  
+  const payForDay = Math.ceil(monthlySalary / 21);
+  const payForHour = Math.ceil(payForDay / 8);
+
+
+  useEffect(() => {
+if(checked === 'monthlySalary') setSalary(monthlySalary)
+if(checked === 'mrot') setSalary(mrot)
+if(checked === 'payForDay') setSalary(payForDay)
+if(checked === 'payForHour') setSalary(payForHour)
+  }, [checked, monthlySalary, payForDay, payForHour]);
+  
+
+const changeInput = (e) => {
+  setmonthlySalary(e.target.value);
+}
+
+  return (
+    <>
+      <div className="containerBoxes">
+        <p className="title">
+          Сумма
+        </p>
+        <div>
+        <input className="salaryMonth" name='1' type="radio" checked={checked === 'monthlySalary'} id="flexCheckDefault" onClick={() => setChecked('monthlySalary')}/>
+          <label class="form-check-label" for="flexCheckDefault">
+          Оклад за месяц
+          </label>
+        </div>
+
+        <div>
+        <input className="mrot" name='1' type="radio" checked={checked === 'mrot'}  id="flexCheckDefault" onClick={() => setChecked('mrot')}/>
+          <label class="form-check-label" for="flexCheckDefault">
+          МРОТ
+          </label>
+        </div>
+
+        <div>
+        <input className="salaryDay" name='1' type="radio" checked={checked === 'payForDay'}  id="flexCheckDefault" onClick={() => setChecked('payForDay')}/>
+          <label class="form-check-label" for="flexCheckDefault">
+          Оплата за день
+          </label>
+        </div>
+
+        <div>
+        <input className="salaryHour" name='1' type="radio" checked={checked === 'payForHour'} id="flexCheckDefault" onClick={() => setChecked('payForHour')} />
+          <label class="form-check-label" for="flexCheckDefault">
+          Оплата за час
+          </label>
+        </div>
+
+
+        <label className="checkbox">
+          <input type="checkbox" className="checkboxInput" checked={toggle} onChange={()=> setToggle(tog => !tog)}/>
+          <span className="checkboxInner"></span>
+        </label>
+
+        <div>
+          <input className="resultInput" type="number" 
+            value={salary} 
+            onChange={changeInput}
+            />
+            <>₽</>
+
+        </div>        
+          {checked === 'monthlySalary' && (
+
+        <div className="resultList">
+          {toggle ? (
+            <>
+              <p> {`${monthlySalary} ₽ сотрудник получает на руки`}</p>
+              <p> {` ${Math.round(monthlySalary / 0.87) - monthlySalary} ₽ НДФЛ, 13% от оклада`}</p>
+              <p> {`${ Math.round(monthlySalary / 0.87) } ₽ за сотрудника в месяц`}</p>
+            </>
+          )
+        :
+        (
+          <>
+            <p> {`${salaryWithoutTax} ₽ сотрудник получает на руки`}</p>
+            <p> {` ${tax} ₽ НДФЛ, 13% от оклада`}</p>
+            <p> {`${monthlySalary} ₽ за сотрудника в месяц`}</p>
+          </>
+        )}
+        </div>
+      )}
       </div>
-
-      <div>
-        2
-      </div>
-
-      <div>
-        3
-      </div>
-
-      <div>
-        4
-      </div>
-
-      <div>
-        5
-      </div>
-      
-    </div>
-
-  </div>
-)
+    </>
+  )
 }
