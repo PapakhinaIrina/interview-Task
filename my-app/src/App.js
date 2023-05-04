c84df888621c66735385d407ba6993fa72c88b44
Ирина <papahinairina@gmail.com>
2023-05-04 14:39:18 +0300

    created app + container

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import Salary from './components/Salary/Salary'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Salary />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Salary/Salary.js
@@ -0,0 +1,35 @@
+import './style.css';
+
+
+
+export default function Salary () {
+
+return (
+  <div className='wrapper'>
+    <div className='container'>
+      <h2> Salary</h2>
+      <div>
+        1
+      </div>
+
+      <div>
+        2
+      </div>
+
+      <div>
+        3
+      </div>
+
+      <div>
+        4
+      </div>
+
+      <div>
+        5
+      </div>
+      
+    </div>
+
+  </div>
+)
+}
